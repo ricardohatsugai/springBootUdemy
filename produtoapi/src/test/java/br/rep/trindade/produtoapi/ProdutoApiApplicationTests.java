16ce1952f9ca90f2b594c76a2e7f8c81295c3275
Ricardo Hatsugai <rhatsugai@gmail.com>
2025-04-26 17:00:26 -0300

    Second commit

---
+++ produtoapi/src/test/java/br/rep/trindade/produtoapi/ProdutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rep.trindade.produtoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
