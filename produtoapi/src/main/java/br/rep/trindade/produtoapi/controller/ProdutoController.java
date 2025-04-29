33e5261d24233eb5113004ac5e1f713708f28078
Ricardo Hatsugai <rhatsugai@gmail.com>
2025-04-29 17:06:04 -0300

    Aula 14 - Persistindo produtos no banco de dados.

--- produtoapi/src/main/java/br/rep/trindade/produtoapi/controller/ProdutoController.java
+++ produtoapi/src/main/java/br/rep/trindade/produtoapi/controller/ProdutoController.java
@@ -1,18 +1,31 @@
 package br.rep.trindade.produtoapi.controller;
 
 import br.rep.trindade.produtoapi.model.Produto;
+import br.rep.trindade.produtoapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
     }
 
